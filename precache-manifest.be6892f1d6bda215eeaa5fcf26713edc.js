c047cd690dcc575a3effa8a78e68ee5e8b8bb108
Unai Bolivar <ujbolivar@gmail.com>
2020-04-25 20:05:09 +0200

    Updates

---
+++ precache-manifest.be6892f1d6bda215eeaa5fcf26713edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256766ee6a00aa35535dce34c3732c17",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "ed0697b7c2ed0619530d",
+    "url": "/Covid19Tracker/static/css/main.6c7f8a8c.chunk.css"
+  },
+  {
+    "revision": "1dfb1f3e53831f61039e",
+    "url": "/Covid19Tracker/static/js/2.d000acab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.d000acab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0697b7c2ed0619530d",
+    "url": "/Covid19Tracker/static/js/main.b5ac5f37.chunk.js"
+  },
+  {
+    "revision": "64face96a235ab578011",
+    "url": "/Covid19Tracker/static/js/runtime-main.63e420d7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/covid image.d7265326.png"
+  }
+]);
